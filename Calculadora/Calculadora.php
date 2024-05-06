0831b7c234d55a8fe96752eb3dfcb2c1c3ec2327
rafaellealdantas <119167570+rafaellealdantas@users.noreply.github.com>
2024-05-06 11:18:39 -0300

    Add files via upload

---
+++ Calculadora/Calculadora.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="site">
+    <div class="cabeçalho">
+        <h1>CALCULADORA PHP</h1>
+    </div>
+    <form action="" method="get">  
+        <div class="balao1"></div>       
+        <input type="text" name="num1" id="num1">
+        
+        <select name="operacao" id="operacao">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>  
+
+        <div class="balao2"></div>
+        <input type="text" name="num2" id="num2">  
+        
+    </form>
+</div>
+</body>
+</html>
+<?php
+$num1 = $_GET['num1'];
+echo $num1
+?>
