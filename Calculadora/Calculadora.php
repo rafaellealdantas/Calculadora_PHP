f499f7ce04dee52ba96ef316c63a8ca610fea446
rafaellealdantas <119167570+rafaellealdantas@users.noreply.github.com>
2024-05-06 14:53:02 -0300

    Add files via upload

--- Calculadora/Calculadora.php
+++ Calculadora/Calculadora.php
@@ -20,17 +20,48 @@
             <option value="-">-</option>
             <option value="*">*</option>
             <option value="/">/</option>
+            <option value="^">^</option>
+            <option value="n!">n!</option>
         </select>  
 
         <div class="balao2"></div>
         <input type="text" name="num2" id="num2">  
         
-        <button id="calcular">calcular</button>
+        <button id="calcular">Calcular</button>
     </form>
-</div>
+
 </body>
 </html>
 <?php
 $num1 = $_GET['num1'];
-echo $num1
+$num2 = $_GET['num2'];
+$operacao = $_GET['operacao'];
+echo '<div class="resultado">';
+switch ($operacao) 
+{
+    case '+':
+        $soma = $num1 + $num2;
+        echo $num1 . " + " . $num2 . " = " .  $soma;
+        break;
+    case '-':
+        $sub = $num1 - $num2;
+        echo $num1 . " - " . $num2 . " = " . $sub;
+        break;
+    case '/':
+        $div = $num1 / $num2;
+        echo $num1 . " / " . $num2 . " = " .  $div;
+        break;
+    case '*':
+        $mult = $num1 * $num2;
+        echo $num1 . " x " . $num2 . " = " .  $mult;
+        break;
+
+    default:
+        echo "operação inválida";
+        break;
+   
+}
+echo '</div>';
 ?>
+
+</div>
